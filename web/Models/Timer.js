86a58d5aa6ca811aceae1aa9b5cc73e488f7e656
shabil15 <mohammedshabil15@gmail.com>
2024-09-24 04:41:40 -0700

    Timer model created

---
+++ web/Models/Timer.js
@@ -0,0 +1,25 @@
+// models/Timer.js
+import mongoose from 'mongoose';
+
+const TimerSchema = new mongoose.Schema({
+  startDateTime: {
+    type: Date,
+    required: true,
+  },
+  endDateTime: {
+    type: Date,
+    required: true,
+  },
+  shop: {
+    type: String, 
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Timer = mongoose.model('Timer', TimerSchema);
+
+export default Timer;
