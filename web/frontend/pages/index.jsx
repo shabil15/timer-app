099576a2ac6c9f7f25bb4a1beaaa73be37f6c704
shabil15 <mohammedshabil15@gmail.com>
2024-09-24 03:30:35 -0700

    Update HomePage to include TimerForm component

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -2,11 +2,13 @@ import {
   Page,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
+import TimerForm from "../components/TimerForm/TimerForm";
 
 export default function HomePage() {
   return (
     <Page narrowWidth>
       <TitleBar title={"Countdown Timer App"} />
+      <TimerForm/>
     </Page>
   );
 }
