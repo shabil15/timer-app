03c4ca7be90208cfd342598b696c9525bd90b0f7
shabil15 <mohammedshabil15@gmail.com>
2024-09-24 06:27:17 -0700

    Initial commit for TimersPage component

---
+++ web/frontend/pages/TimersPage.jsx
@@ -0,0 +1,16 @@
+import {
+    Page,
+  } from "@shopify/polaris";
+  import { TitleBar } from "@shopify/app-bridge-react";
+  import TimerForm from "../components/TimerForm/TimerForm";
+  
+  export default function TimersPage() {
+    return (
+      <Page narrowWidth>
+        <TitleBar title={"Timers"} />
+        {/* <h1>Welcome to the Countdown Timer App.</h1> */}
+        <TimerForm/>
+      </Page>
+    );
+  }
+  
